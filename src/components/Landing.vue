85138823259774094b398d5b5567a56ae37386e7
dapobelieve <dapomichaels@gmail.com>
2019-06-25 20:30:28 +0100

    updates to home page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="landing text-left flex flex-col justify-end" style="background: linear-gradient(89.81deg, #111111 0.05%, rgba(17, 17, 17, 0.65) 41.25%), url('https://res.cloudinary.com/dldd8ucby/image/upload/v1561318349/Bitmap.png') no-repeat center center;">
-  <div class="lower-content p-16 "> 
+  <div class="lower-content p-16 ">
           <p class="uppercase text-4xl text-white font-bold ">ADEKUfffNLE GOLD LIVE IN CONCERT</p>
     <p class="text-xl opacity-75 text-white font-semibold ">22nd August 2019 | 1400hrs</p>
     <div class="">
-            <button class="bg-green-500 text-white font-bold py-2 px-4 rounded">Watch Stream</button>
+            <button class="bg-green-500 text-white font-bold py-2 px-4 focus:outline-none rounded">Watch Stream</button>
     <button class="bg-transparent-500 m-2 text-white border hover:bg-white hover:text-green-500 border-solid border-white-800 font-bold py-2 px-4 rounded">Add to Calendar</button>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="home">
     <Landing/>
-    <SingleEvent/>
+    <div class="mx-16 mt-16">
+      <!-- mini nav area -->
+      <nav class="flex items-start border-b border-gray-400 mb-10">
+        <div class="flex justify-between -mb-px ">
+          <a href="#" class="px-3 pb-2 text-green-500 font-semibold tracking-normal border-b-2 border-green-700">Live Events</a>
+          <a href="#" class="px-3 pb-2 text-gray-500 ml-6 border-b border-gray-400 tracking-normal font-semibold hover:border-green-700 hover:text-green-500">Upcoming</a>
+          <a href="#" class="px-3 pb-2 text-gray-500 border-gray-400 ml-6 border-b tracking-normal font-semibold hover:border-green-700 hover:text-green-500">Past Events</a>
+        </div>
+      </nav>
+
+      <div class="events flex">
+        <div class="w-1/4 flex">
+          <div class="bg-white shadow-xl rounded">
+            <div>
+              <img class="h-64 w-full " src="https://res.cloudinary.com/invitro/image/upload/v1561296154/9jastream/hx3b190wbxh62fq2bnir.jpg">              
+            </div>
+            <div class="my-3 ">
+              <span class="font-bold text-black flex justify-start ml-4">Simi London Concert</span>
+              <p class="flex justify-start ml-4 font-bold text-sm text-gray-600 tracking-wide">June 24th 2019 | 1700hrs</p>
+            </div>            
+          </div>
+        </div>
+      </div>
+    </div>
     <Footer/>
-    
   </div>
 </template>
 
@@ -12,6 +34,7 @@
 import Landing from '@/components/Landing.vue';
 import SingleEvent from '@/components/SingleEvent.vue';
 import Footer from '@/components/Footer.vue';
+
 export default {
   name: 'home',
   components: {
