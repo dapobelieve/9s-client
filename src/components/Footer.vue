03c660e635cf21f58aecfc1d3a989fcb91270d64
Benjamin Alamu: Codeblooded <oluwaseunalamu@gmail.com>
2019-06-27 08:05:22 +0100

    Respomsive footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="bg-white">
     <div class="mx-16 py-8">
-      <div class="flex justify-between border-b border-gray-300 pb-2">
+      <div class="block md:flex justify-between border-b border-gray-300 pb-2">
         <div class="uppercase text-3xl items-center">
           <span class="font-thin tracking-tighter">9JA</span>
           <span class="font-black tracking-tighter">STREAM</span>
         </div>
-        <div class="flex justify-between">
-          <div class="cursor-pointer">About</div>
+        <div class="block text-center md:flex jmd:ustify-between">
+          <div class="cursor-pointer ml-6">About</div>
           <div class="cursor-pointer ml-6">Events</div>
           <div class="cursor-pointer ml-6">Contacts</div>
         </div>
       </div>
-      <div class="flex mt-4 justify-between items-center">
+      <div class="block md:flex mt-4 justify-between items-center">
         <div>Copyright 2019. All rights reserved</div>
-        <div class="flex justify-between">
+        <div class="block md:flex mt-2 justify-between">
           <span class=""><i class="fab fa-facebook fa-2x"></i></span>
           <span class="ml-3"><i class="fab fa-twitter-square fa-2x"></i></span>
           <span class="ml-3"><i class="fab fa-instagram fa-2x"></i></span>
