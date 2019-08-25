27250cd7dcfa0ea70d88c4544089fc2ddf536e82
dapobelieve <dapomichaels@gmail.com>
2019-08-25 11:59:24 +0100

    added add event link

--- src/admin/parts/sidebar.vue
+++ src/admin/parts/sidebar.vue
@@ -8,6 +8,11 @@
                   </router-link>
               </li>
               <li class="mr-3 flex-1">
+                  <router-link :to="{name: 'add-event'}" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
+                      <i class="fas fa-video pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Add Event</span>
+                  </router-link>
+              </li>
+              <li class="mr-3 flex-1">
                   <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
                       <i class="fa fa-users pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Users</span>
                   </a>
